7c84bf8e400b8052bfd0196156abad194d35988a
theshrish46 <theshrish46@gmail.com>
2023-08-10 09:59:02 +0530

    added hero page

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,67 +1,84 @@
-import React from 'react'
-import { BsFillPrinterFill, BsFillFolderFill, BsFolderFill } from 'react-icons/bs'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { CgProfile } from 'react-icons/cg'
-import { TbDeviceLandlinePhone } from 'react-icons/tb'
+import React from 'react';
+import {
+  BsFillPrinterFill,
+  BsFillFolderFill,
+  BsFolderFill,
+} from 'react-icons/bs';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { CgLogOff, CgProfile } from 'react-icons/cg';
+import { TbDeviceLandlinePhone } from 'react-icons/tb';
+
+import logo from './../public/assets/logo/logo.png';
+import Image from 'next/image';
 
 const Header = () => {
-    return (
-        <>
-            <div className='w-full mx-auto px-4 my-6
-            flex items-center justify-between
-            '>
-                <div className='flex justify-center items-center
+  return (
+    <>
+      <div
+        className='w-full mx-auto px-4 mb-10 mt-5
+            flex items-center justify-around
+            '
+      >
+        <div
+          className='flex justify-center items-center
                     gap-3 mr-8
-                '>
-                    <BsFillPrinterFill size={30} />
-                    <span className='font-bold text-2xl italic text-blue-400'>Print Universe</span>
-                </div>
-                <div className='
+                '
+        >
+          <Image src={logo} className='w-52 h-10' />
+        </div>
+        <div
+          className='
                     w-5/12 mx-4
-                '>
-                    <input type="search" placeholder='Search'
-                        className='
+                '
+        >
+          <input
+            type='search'
+            placeholder='Search'
+            className='
                         px-4 py-3 placeholder:italic focus:outline-none
                         border-2 border-gray-500 rounded-md w-full
-                    '/>
-                </div>
-                <div className='
+                    '
+          />
+        </div>
+        <div
+          className='
                     mx-4
-                '>
-                    <ul className='flex justify-between items-center w-full'>
-                        <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer hover:underline'>
-                            <TbDeviceLandlinePhone className='w-8 h-8' />
-                            <div className='flex flex-col'>
-                                <span className='text-sm'>Help Is Here</span>
-                                <span className='text-xs'>1234 5678 9012</span>
-                            </div>
-                        </li>
+                '
+        >
+          <ul className='flex justify-between items-center w-full'>
+            <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer hover:underline'>
+              <TbDeviceLandlinePhone className='w-8 h-8' />
+              <div className='flex flex-col'>
+                <span className='text-sm'>Help Is Here</span>
+                <span className='text-xs'>1234 5678 9012</span>
+              </div>
+            </li>
 
-                        <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer hover:underline'>
-                            <BsFolderFill className='w-6 h-6' />
-                            <div className='flex flex-col text-sm'>
-                                <span>My Project</span>
-                            </div>
-                        </li>
+            <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer hover:underline'>
+              <BsFolderFill className='w-6 h-6' />
+              <div className='flex flex-col text-sm'>
+                <span>My Project</span>
+              </div>
+            </li>
 
-                        <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer hover:underline'>
-                            <CgProfile className='w-6 h-6' />
-                            <div className='flex flex-col text-sm'>
-                                <span>Sign In</span>
-                            </div>
-                        </li>
+            <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer hover:underline'>
+              <CgProfile className='w-6 h-6' />
+              <div className='flex flex-col text-sm'>
+                <span>Sign In</span>
+              </div>
+            </li>
 
-                        <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer'>
-                            <AiOutlineShoppingCart className='w-6 h-6' />
-                            <div className='flex flex-col text-sm'>
-                                <span>Cart</span>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
-}
+            <li className='flex justify-around items-center gap-3 mx-4 cursor-pointer'>
+              <AiOutlineShoppingCart className='w-6 h-6' />
+              <div className='flex flex-col text-sm'>
+                <span>Cart</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
--- frontend/components/HeroPage.jsx
+++ frontend/components/HeroPage.jsx
@@ -1,17 +1,22 @@
-"use client"
+'use client';
 import React from 'react';
-import bgone from './../public/assets/bgs/bgone.webp'
+import bgone from './../public/assets/bgs/bgone.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const HeroPage = () => {
     return (
         <>
-            <div id='bgone' className='w-full h-full' style={{
-                width: '100%',
-                height: '100%',
-                backgroundImage: `url(${bgone})`
-            }}>
-                <div className="w-full h-full">
+            <div id='bgone' className='w-full h-[33rem] flex justify-start items-center px-10'>
+                <div className='flex justify-center items-center'>
+                    <div>
+                        <h3 className='mb-8 text-5xl text-white'>My Name, My Pride</h3>
+                        <p className='mb-6 text-2xl text-white'>100 Visiting cards at Rs 180</p>
+                        <Link href={'#'} className='mb-6 bg-white px-8 py-4 rounded-3xl'>
+                            Shop Now
+                        </Link>
+                        <p className='my-6 text-white text-xs'>Weather condition may affect the delivery timelines.</p>
+                    </div>
                 </div>
             </div>
         </>
